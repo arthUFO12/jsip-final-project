d40c926eab96a0ec90a4126b9a46e2c823c51f94
nigeltatem2@gmail.com <ubuntu@ip-172-31-29-102.us-east-2.compute.internal>
2026-07-21 21:05:21 +0000

    basic types for exchange_parser & function layouts

---
+++ lib/types/src/price.ml
@@ -0,0 +1,48 @@
+open! Core
+
+module T = struct
+  type t = int [@@deriving sexp, bin_io, compare, equal, hash]
+  (* Invariant: value is in microcents. *)
+end
+
+include T
+include Comparable.Make (T)
+
+let microcents_per_cent = 1_000_000
+let microcents_per_dollar = 100_000_000
+let of_microcents n = n
+let of_int_cents n = Int.( * ) n microcents_per_cent
+let to_microcents t = t
+
+let to_int_cents_exn t =
+  if Int.( <> ) (Int.rem t microcents_per_cent) 0
+  then
+    raise_s
+      [%message
+        "Price.to_int_cents_exn: not a whole number of cents" (t : int)];
+  Int.( / ) t microcents_per_cent
+;;
+
+let to_dollar_float t =
+  Float.( / ) (Float.of_int t) (Float.of_int microcents_per_dollar)
+;;
+
+let zero = 0
+let ( + ) = Int.( + )
+let ( - ) = Int.( - )
+let ( * ) price qty = Int.( * ) price qty
+
+let to_string_dollar t =
+  let is_negative = Int.( < ) t 0 in
+  let t_abs = Int.abs t in
+  let dollars = Int.( / ) t_abs microcents_per_dollar in
+  let frac = Int.rem t_abs microcents_per_dollar in
+  let frac_str =
+    let full = sprintf "%08d" frac in
+    let trimmed = String.rstrip full ~drop:(Char.( = ) '0') in
+    if Int.( < ) (String.length trimmed) 2
+    then String.prefix full 2
+    else trimmed
+  in
+  sprintf "%s$%d.%s" (if is_negative then "-" else "") dollars frac_str
+;;
