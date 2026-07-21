e10ddd3a6048a2dab0bfe6c56624f3409d40359a
nigeltatem2@gmail.com <ubuntu@ip-172-31-29-102.us-east-2.compute.internal>
2026-07-21 16:31:18 +0000

    cleanup

--- types/src/odds.mli
+++
@@ -1,9 +0,0 @@
-
-
-
-type t = float
-
-
-val of_float : float -> t
-val to_float : t -> float
-
--- types/src/price.mli
+++
@@ -1,9 +0,0 @@
-
-
-type t = int 
-
-
-
-
-val of_int_cents : int -> t
-val of_float_exn : float -> t
